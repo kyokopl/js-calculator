3b25cad656c99ba952cc00e46777aceac3b22028
Kyoko Pohe <Kyokz@Kyokos-MacBook-Pro.local>
2018-08-02 11:39:38 +1200

    calculator functions

--- calculator.js
+++ calculator.js
@@ -1,70 +1,54 @@
-var entries = [];
-var total = 0;
-
+var screenDisplay = '';
 var temp = '';
-$("button").on('click', function() {
- 	var val = $(this).text();
 
-  // Got a number, add to temp
-  if (!isNaN(val) || val === '.') {
-    temp += val;
-    $("#answer").val(temp.substring(0,10));
-    
-  // Got some symbol other than equals, add temp to our entries
-  // then add our current symbol and clear temp
-  } else if (val === 'AC') {
-    entries = [];
-    temp = '';
-    total = 0;
-    $("#answer").val('')
+function clickNum(i) { 
+    checkForNewCalculation();
+    displayNum(i);
+}
 
-  // Clear last entry
-  } else if (val === 'CE') {
-    temp = '';
-    $("#answer").val('')
-    
-  // Change multiply symbol to work with eval
-  } else if (val === 'x') {
-    entries.push(temp);
-    entries.push('*');
-    temp = '';
-    
-  // Change divide symbol to work with eval
-  } else if (val === '÷') {
-    entries.push(temp);
-    entries.push('/');
-    temp = '';
+// Display number on screen
+function displayNum(i) {
+    document.getElementById("screen").value += i;
+    temp += i;
+}
 
-  // Got the equals sign, perform calculation
-  } else if (val === '=') {
-  	entries.push(temp);
+// Check for new calculation
+function checkForNewCalculation() {
+    screenDisplay = document.getElementById("screen").value;
+    if (document.getElementById("screen").classList.contains("result")){
+        allClear();
+        document.getElementById("screen").classList.remove("result");
+    };
+}
 
-    var nt = Number(entries[0]);
-    for (var i = 1; i < entries.length; i++) {
-      var nextNum = Number(entries[i+1])
-      var symbol = entries[i];
-      
-      if (symbol === '+') { nt += nextNum; } 
-      else if (symbol === '-') { nt -= nextNum; } 
-      else if (symbol === '*') { nt *= nextNum; } 
-      else if (symbol === '/') { nt /= nextNum; }
-      
-      i++;
-    }
-    
-    // Swap the '-' symbol so text input handles it correctly
-    if (nt < 0) {
-      nt = Math.abs(nt) + '-';
-    }
-    
-    $("#answer").val(nt);
-		entries = [];
-    temp = '';
-    
-  // Push number
-  } else {
-    entries.push(temp);
-    entries.push(val);
-    temp = '';
-  }
-});
+// Clear for C
+function clearScreen() {
+    screenDisplay = "";
+    document.getElementById("screen").value = screenDisplay;
+}
+
+// Clear for AC
+function allClear() {
+    screenDisplay = "";
+    document.getElementById("screen").value = screenDisplay;
+    temp ="";
+}
+
+// Performing calculations
+function maths(i) {
+    screenDisplay =document.getElementById("screen");
+    if (document.getElementById("screen").classList.contains("result")){
+        temp = screenDisplay.value;
+        screenDisplay.classList.remove("result");
+    };
+    clearScreen();
+    temp += i;
+}
+
+// Result 
+function equal() {
+    screenDisplay = document.getElementById("screen");
+    temp = eval(temp);
+    document.getElementById("screen").value = temp;
+    screenDisplay.classList.add("result");
+}
